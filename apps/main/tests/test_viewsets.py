69ba841e8e77be9e40de30396c5f27a97c03b002
Vadim Laletin <rusmiligamer@gmail.com>
2017-11-10 14:53:41 +0700

    Change add_transition_actions decorator. Add support for custom serializers

--- apps/main/tests/test_viewsets.py
+++ apps/main/tests/test_viewsets.py
@@ -12,18 +12,22 @@ from ..viewsets import add_transition_actions
 class Simple(models.Model):
     title = models.CharField()
 
+
 class SimpleModelSerializers(serializers.ModelSerializer):
     class Meta:
         model = Simple
         fields = ('title', )
 
+
 class SimpleModelViewSet(viewsets.GenericViewSet):
     queryset = Simple.objects.none()
     serializer_class = SimpleModelSerializers()
 
+
 class TwoFSMField(models.Model):
     state_one = FSMField(default='a')
     state_two = FSMField(default='b')
+
 
 class TwoFSMFieldSerializers(serializers.ModelSerializer):
     class Meta:
@@ -32,9 +36,11 @@ class TwoFSMFieldSerializers(serializers.ModelSerializer):
             'state_on',
             'state_two', )
 
+
 class TwoFSMFieldViewSet(viewsets.GenericViewSet):
     queryset = TwoFSMField.objects.all()
     serializer_class = TwoFSMFieldSerializers()
+
 
 class NormalFSM(models.Model):
     state = FSMField(default='a')
@@ -56,10 +62,12 @@ class NormalFSM(models.Model):
     def c_to_d(self):
         pass
 
+
 class NormalFSMModelSerializers(serializers.ModelSerializer):
     class Meta:
         model = NormalFSM
         fields = ('state', )
+
 
 class NormalFSMModelViewSet(viewsets.GenericViewSet):
     queryset = NormalFSM.objects.none()
@@ -81,3 +89,11 @@ class AddTransitionActionsDecorator(TestCase):
         self.assertTrue(callable(NewViewSet.b_to_c))
         self.assertTrue(callable(NewViewSet.a_to_c))
         self.assertFalse(hasattr(NewViewSet, 'c_to_d'))
+
+    def test_that_you_can_specify_custom_serializers(self):
+        NewViewSet = add_transition_actions(
+            serializers={'a_to_b': serializers.Serializer}
+        )(NormalFSMModelViewSet)
+        self.assertTrue(callable(NewViewSet.a_to_b))
+        self.assertTrue(callable(NewViewSet.b_to_c))
+        self.assertTrue(callable(NewViewSet.a_to_c))
--- apps/main/viewsets.py
+++ apps/main/viewsets.py
@@ -1,4 +1,6 @@
 from copy import copy
+from functools import wraps
+
 from django.core.exceptions import ImproperlyConfigured
 from django.db import transaction
 
@@ -10,51 +12,67 @@ from django_fsm import (
     can_proceed, has_transition_perm, FSMFieldMixin, TransitionNotAllowed, )
 
 
-def add_transition_actions(Klass):
-    Model = Klass.queryset.model
-    fsm_fields = [f.name for f in Model._meta.fields
-                  if isinstance(f, FSMFieldMixin)]
-    if len(fsm_fields) == 0:
-        raise ImproperlyConfigured(
-            "There is no FSM field at {0}".format(Model))
+def add_transition_actions(*decorator_args, **decorator_kwargs):
+    serializers = decorator_kwargs.get('serializers', {})
 
-    if len(fsm_fields) > 1:
-        raise ImproperlyConfigured(
-            "There is more than one FSM field at {0}".format(Model))
+    def decorator(Klass):
+        Model = Klass.queryset.model
+        fsm_fields = [f.name for f in Model._meta.fields
+                      if isinstance(f, FSMFieldMixin)]
+        if len(fsm_fields) == 0:
+            raise ImproperlyConfigured(
+                "There is no FSM field at {0}".format(Model))
 
-    method_name = "get_all_{0}_transitions".format(fsm_fields[0])
+        if len(fsm_fields) > 1:
+            raise ImproperlyConfigured(
+                "There is more than one FSM field at {0}".format(Model))
 
-    for f in getattr(Model, method_name)(Model()):
-        # Skip methods which explicitly excluded via using `viewset=False`
-        # in custom attr
-        if not f.custom.get('viewset', True):
-            continue
+        method_name = "get_all_{0}_transitions".format(fsm_fields[0])
 
-        def get_fn(name_):
-            name = copy(name_)
+        for f in getattr(Model, method_name)(Model()):
+            # Skip methods which explicitly excluded via using `viewset=False`
+            # in custom attr
+            if not f.custom.get('viewset', True):
+                continue
 
-            @transaction.atomic
-            def fn(self, request, **kwargs):
-                args = self.get_serializer(data=request.data)
-                args.is_valid(raise_exception=True)
-                obj = self.get_object()
-                transition = getattr(obj, name)
-                if can_proceed(transition) and \
-                        has_transition_perm(transition, request.user):
-                    try:
-                        transition(**args.validated_data)
-                    except TransitionNotAllowed as err:
-                        raise ValidationError(str(err))
-                    obj.save()
-                    return Response(status=204)
-                else:
-                    raise ValidationError(
-                        "You cant perform '{}' transition".format(name))
-            return fn
+            def get_fn(name_):
+                name = copy(name_)
 
-        serializer_class = f.custom.get('args_serializer') or Serializer
+                @transaction.atomic
+                def fn(self, request, **kwargs):
+                    args = self.get_serializer(data=request.data)
+                    args.is_valid(raise_exception=True)
+                    obj = self.get_object()
+                    transition = getattr(obj, name)
+                    if can_proceed(transition) and \
+                            has_transition_perm(transition, request.user):
+                        try:
+                            transition(**args.validated_data)
+                        except TransitionNotAllowed as err:
+                            raise ValidationError(str(err))
+                        obj.save()
+                        return Response(status=204)
+                    else:
+                        raise ValidationError(
+                            "You cant perform '{}' transition".format(name))
+                return fn
 
-        setattr(Klass, f.name, detail_route(
-            methods=['post'],
-            serializer_class=serializer_class)(get_fn(f.name)))
-    return Klass
+            serializer_class = serializers.get(f.name, Serializer)
+
+            setattr(Klass, f.name, detail_route(
+                methods=['post'],
+                # Skip permissions because transition has owns
+                permission_classes=(),
+                serializer_class=serializer_class)(get_fn(f.name)))
+        return Klass
+
+    # Add an ability to use decorator without arguments
+    # For example:
+    # @add_transition_actions
+    # class MyViewSet:
+    # ...
+    if len(decorator_args) == 1 and callable(decorator_args[0]) and \
+            not decorator_kwargs:
+        return decorator(decorator_args[0])
+
+    return decorator
