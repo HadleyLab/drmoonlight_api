42853a73a161acda3229470058d5ce560c1d95e4
Ilya Beda <ir4y.ix@gmail.com>
2017-11-24 12:32:36 +0300

    Fix permissions for interceptors entrypoint

--- apps/accounts/views/constants.py
+++ apps/accounts/views/constants.py
@@ -1,10 +1,12 @@
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 
 from apps.accounts.models import ResidencyProgram, Speciality
 
 
 @api_view()
+@permission_classes([AllowAny])
 def constants(request):
     residency_program = ResidencyProgram.objects.values('pk', 'name')
     speciality = Speciality.objects.values('pk', 'name')
