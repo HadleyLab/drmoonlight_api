266b81eb5f4a024a9bdce5b5980a226238289f03
Ilya Beda <ir4y.ix@gmail.com>
2017-11-29 13:45:59 +0300

    Add wating_for_approve route to ResidentViewSet

--- apps/accounts/viewsets/resident.py
+++ apps/accounts/viewsets/resident.py
@@ -1,8 +1,10 @@
 from djoser import email
 from djoser.compat import get_user_email
 from rest_framework import viewsets, mixins
+from rest_framework.decorators import list_route
+from rest_framework.response import Response
 
-from apps.accounts.models import Resident
+from apps.accounts.models import Resident, ResidentStateEnum
 from apps.accounts.permissions import ResidentPermission
 from apps.accounts.serializers import (
     ResidentCreateSerializer, ResidentUpdateSerializer,
@@ -28,6 +30,15 @@ class ResidentViewSet(mixins.CreateModelMixin,
             return ResidentUpdateSerializer
 
         return self.serializer_class
+
+    @list_route()
+    def wating_for_approve(self, request):
+        user = request.user
+        if not user.is_authenticated or not user.is_account_manager:
+            return Response(status=403)
+        qs = Resident.objects.filter(state=ResidentStateEnum.PROFILE_FILLED)
+        serializer = self.get_serializer(qs, many=True)
+        return Response(serializer.data)
 
     def perform_create(self, serializer):
         user = serializer.save()
