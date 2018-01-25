a8d805044bc12d10d01402ae6257063960383296
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-01-25 17:55:51 +0700

    user avatar in messages

--- apps/shifts/serializers/message.py
+++ apps/shifts/serializers/message.py
@@ -4,7 +4,11 @@ from apps.shifts.models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
+
+    owner_avatar = serializers.ImageField(source='owner.avatar')
+
     class Meta:
         model = Message
-        fields = ('pk', 'date_created', 'owner', 'application', 'text', )
+        fields = ('pk', 'date_created', 'owner', 'owner_avatar',
+                  'application', 'text', )
         read_only_fields = ('owner', 'application', )
