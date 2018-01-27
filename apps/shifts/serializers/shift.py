8967843358b8dca3b275e398a52de003e406a556
Ilya Beda <ir4y.ix@gmail.com>
2018-01-27 16:22:34 +0300

    Ref #37 Add was_rejected param for shift

--- apps/shifts/serializers/shift.py
+++ apps/shifts/serializers/shift.py
@@ -2,12 +2,13 @@ from django.utils import timezone
 from rest_framework import serializers
 
 from apps.accounts.serializers import SchedulerSerializer
-from apps.shifts.models import Shift
+from apps.shifts.models import Shift, ApplicationStateEnum
 
 
 class ShiftSerializer(serializers.ModelSerializer):
     owner = SchedulerSerializer(read_only=True)
     has_already_applied = serializers.SerializerMethodField()
+    was_rejected = serializers.SerializerMethodField()
 
     class Meta:
         model = Shift
@@ -15,7 +16,7 @@ class ShiftSerializer(serializers.ModelSerializer):
             'pk', 'owner', 'date_created', 'date_modified', 'date_end',
             'date_start', 'speciality',
             'residency_years_required', 'payment_amount', 'payment_per_hour',
-            'description', 'state', 'has_already_applied', )
+            'description', 'state', 'has_already_applied', 'was_rejected', )
 
     def get_has_already_applied(self, obj):
         request = self.context['request']
@@ -25,11 +26,29 @@ class ShiftSerializer(serializers.ModelSerializer):
         if user.is_resident:
             # `applications.all()` are already prefetched
             applicants_pks = [
-                application.owner.pk
+                application.owner_id
                 for application in obj.applications.all()]
             return user.pk in applicants_pks
 
         return None
+
+    def get_was_rejected(self, obj):
+        request = self.context['request']
+
+        user = request.user
+
+        if user.is_resident:
+            # `applications.all()` are already prefetched
+            applications = [
+                application
+                for application in obj.applications.all()
+                if application.owner_id == user.pk]
+            if len(applications) > 0:
+                return applications[0].state == ApplicationStateEnum.REJECTED
+
+        return None
+
+
 
     def validate(self, attrs):
         attrs = super(ShiftSerializer, self).validate(attrs)
