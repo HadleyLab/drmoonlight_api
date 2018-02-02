088ff9c71be8e50a4ec04c1591a5105fabf871fe
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-02-02 14:41:17 +0700

    comment attachment field in serializer

--- apps/shifts/serializers/message.py
+++ apps/shifts/serializers/message.py
@@ -1,16 +1,17 @@
 from rest_framework import serializers
 
 from apps.main.utils import get_avatar_thumbnail
-from apps.main.validators import validate_file_size
+# from apps.main.validators import validate_file_size
 from apps.shifts.models import Message
 
 
 class MessageSerializer(serializers.ModelSerializer):
 
     owner_avatar = serializers.SerializerMethodField()
-    attachment = serializers.FileField(
-        validators=[validate_file_size],
-        required=False)
+    # TODO uncomment or delete when take feedback
+    # attachment = serializers.FileField(
+    #     validators=[validate_file_size],
+    #     required=False)
     thumbnail = serializers.ImageField(read_only=True)
 
     def get_owner_avatar(self, obj):
